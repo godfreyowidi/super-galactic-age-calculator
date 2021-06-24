81b7c16444b616f29c26aa1c844feb2d5f75e2ed
Shoshana Newman <sho.newman@gmail.com>
2021-06-24 09:54:27 -0700

    UAdd function that takes 2 last digits of a year and divdies it by 4
    Co-Authored-By: Godfrey Owidi<godfreyowiidi@gmail.com>

--- __test__/date.test.js
+++ __test__/date.test.js
@@ -1,8 +1,14 @@
 import { leapYear } from './../src/date.js';
+import { yearCode } from './../src/date.js';
 
 describe('leapYear', () => {
-
   test('should test if user input for year is a leap year', () => {
     expect(leapYear(2020)).toEqual(true);
+  });
+})
+
+describe('yearCode', () => {
+  test('should test that function returns the last two numbers in the year input', () => {
+    expect(yearCode('2061')).toEqual(15);
   });
 })
--- src/date.js
+++ src/date.js
@@ -4,6 +4,18 @@ export function leapYear(yy) {
   }
 };
 
+
+export function yearCode(year) {
+  let yy = Math.floor((parseInt(year.slice(year.length - 2))) / 4);
+  return yy;
+}
+
+// 2. Code for every year
+//   1. Take last 2 digits of any year
+//   2. Divide by 4 and ignore the remainder
+//   3. If no remainder its a Leap year and the January code is 5 and Febuary code is 1
+//   4. Then add that answer to the last 2 digits of the year
+//   5. Divide by 7 and the remainder is the code
 //export const dateCodeObj = {
 //   Monday: 1,
 //   Tuesday: 2, 
