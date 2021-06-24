4042d8bd77f2e4495561ac2d0cb5dc3c9583fb62
Shoshana Newman <sho.newman@gmail.com>
2021-06-24 13:56:06 -0700

    Add user inteface logic and form input fields
    Co-Authored-By: Godfrey Owidi<godfreyowiidi@gmail.com>

--- src/date.js
+++ src/date.js
@@ -62,36 +62,5 @@ export function dayCodeConverter(dayCode) {
   return day;
 }
 
-// Month Code (get from monthCodeObj) + Day (get from user Input) + Year Code (We have to calculate) = Answer / 7 remainder is day of week
 
-
-
-// const monthCodeArray = [6, 2, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4]
-
-
-// export function monthCodeGenerator(month) {
-//   let monthCode;
-//   if (month === January)
-//   return monthCode = 6;
-// } else if (month === February) {
-//   return monthCode = 2;
-// }
-
-// 2. Code for every year
-//   1. Take last 2 digits of any year
-//   2. Divide by 4 and ignore the remainder
-//   3. If no remainder its a Leap year and the January code is 5 and Febuary code is 1
-//   4. If its not a leap year then add that answer to the last 2 digits of the year
-//   5. Divide by 7 and the remainder is the code
-//export const dateCodeObj = {
-//   Monday: 1,
-//   Tuesday: 2, 
-//   Wednesday: 3,
-//   Thursday: 4,
-//   Friday: 5,
-//   Saturday: 6,
-//   Sunday: 7
-// }
-
-// Test for LeapYear
 
--- src/main.js
+++ src/main.js
@@ -8,5 +8,14 @@ $(document).ready(function() {
   $('#date').submit(function(event){
 
     event.preventDefault();
+    const inputtedMonth = $("#month-input").val();
+    const inputtedYear = $("#year-input").val();
+    const inputtedDate = $("date-input").val();
+
+    let date = new Date(inputtedMonth, inputtedYear, inputtedDate);
+    let Month = monthCodeGenerator(inputtedMonth);
+    let Year = yearCodeGenerator(inputtedYear);
+    let dayOfTheWeek = date.tabulate();
+    $("#dayOfTheWeek").append()
   });
 });
