223d3e0d50cc0398a93153e8b289a4814c3f8b9e
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-06-25 20:19:49 +0300

    add initial files

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'weekday-calculator',
+      title: 'super-galactic-age-calculator',
       template: './src/index.html',
       inject: 'body'
     })
