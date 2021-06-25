5bc1262791afba1d1073a27418a8bcb09d94b61c
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-06-26 00:41:49 +0300

    add function to log a value based on the sex of the user

--- __test__/life.test.js
+++ __test__/life.test.js
@@ -1,7 +1,18 @@
 import { lifeExpectancyBasedOnAge } from './../src/life.js';
+import { lifeExpectancyBasedOnSex } from './../src/life.js';
 
 describe('lifeExpectancyBasedOnAge', () => {
   test('should test function that divides the user inputted age by 2', () => {
     expect(lifeExpectancyBasedOnAge(10)).toEqual(5);
   });
 })
+
+describe('lifeExpectancyBasedOnSex', () => {
+  test('should test function that provides a value based on user sex', () => {
+    expect(lifeExpectancyBasedOnSex("male")).toEqual(30);
+  });
+})
+
+
+
+
--- src/life.js
+++ src/life.js
@@ -6,3 +6,14 @@ export default function life(age, sex) {
 export function lifeExpectancyBasedOnAge(age) {
     return (age / 2);
 }
+
+export function lifeExpectancyBasedOnSex(sex) {
+  let estimate;
+  if (sex === "male") {
+    return estimate = 30;
+  } else if (sex === "female") {
+    return estimate = 40;
+  } else {
+    return "No Pun Intended!"
+  }
+}
