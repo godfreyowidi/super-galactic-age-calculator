b7c40e97aaa8910fdfbee74b4bff8d9a8e0ac18e
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-06-25 21:54:25 +0300

    should test function that converts user inputted age to venusAge age

--- __test__/age.test.js
+++ __test__/age.test.js
@@ -1,7 +1,15 @@
 import { mercuryAge } from './../src/age.js';
+import { venusAge } from './../src/age.js';
 
 describe('mercuryAge', () => {
-  test('should test if value of ages object is captured', () => {
+  test('should test function that converts user inputted age to mercury age', () => {
     expect(mercuryAge(10)).toEqual("You are 41.66666666666667 mercury years old");
+  });
+})
+
+
+describe('venusAge', () => {
+  test('should test function that converts user inputted age to venusAge age', () => {
+    expect(venusAge(10)).toEqual("You are 16.129032258064516 venus years old");
   });
 })
--- src/age.js
+++ src/age.js
@@ -12,5 +12,9 @@ let ages = {
 export function mercuryAge(number) {
     let age = ((number) / (ages.mercury));
     return ("You are " + age + " mercury years old")
-  
+}
+
+export function venusAge(number) {
+  let age = ((number) / (ages.venus));
+  return ("You are " + age + " venus years old")
 }
