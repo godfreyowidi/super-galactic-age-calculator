882177e8b303a462fd5982d2c62ccc2f420c194c
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-07-04 20:41:50 +0300

    add test that converts user inputted test to mercury years

--- __test__/age.test.js
+++ __test__/age.test.js
@@ -1,42 +1,8 @@
-/*import { galacticAge } from './../src/age.js';
-
-describe('galacticAge', () => {
-  test('should test function that calculate galactic age using solar years', () => {
-    const mercury_age = new Age(10);
-    expect(mercury_age.mercuryAge(10)).toEqual("You are 41.66666666666667 mercury years old");
-  });
-})
-*/
-
 import Age, { mercuryAge } from './../src/age.js';
-import Age, { venusAge } from './../src/age.js';
-import Age, { marsAge } from './../src/age.js';
-import Age, { jupiterAge } from './../src/age.js';
 
 describe('mercuryAge', () => {
   test('should test function that converts user inputted age to mercury age', () => {
     const mercury_age = new Age(10);
     expect(mercury_age.mercuryAge(10)).toEqual("You are 42 mercury years old");
-  });
-})
-
-describe('venusAge', () => {
-  test('should test function that converts user inputted age to venusAge age', () => {
-    const venus_age = new Age(10);
-    expect(venus_age.venusAge(10)).toEqual("You are 16 venus years old");
-  });
-})
-
-describe('marsAge', () => {
-  test('should test function that converts user inputted age to marsAge age', () => {
-    const mars_age = new Age(10);
-    expect(mars_age.marsAge(10)).toEqual("You are 6 mars years old");
-  });
-})
-
-describe('jupiterAge', () => {
-  test('should test function that converts user inputted age to jupiterAge age', () => {
-    const jupiter_age = new Age(10);
-    expect(jupiter_age.jupiterAge(10)).toEqual("You are 1 jupiter years old");
   });
 })
--- src/age.js
+++ src/age.js
@@ -1,30 +1,9 @@
 export default class Age {
-constructor(age, lifeExpectancy) {
-  this.age = age;
-  this.lifeExpectancy = 100;
-}
+  constructor(age) {
+    this.age = age;
+  }
 
   mercuryAge(number) {
-    let age = Math.round((number) / (0.24));
-    return ("You are " + age + " mercury years old")
+    
   }
-
-  venusAge(number) {
-    let age = Math.round((number) / (0.62));
-    return ("You are " + age + " venus years old")
-  }
-
-  marsAge(number) {
-    let age = Math.round((number) / (1.8));
-    return ("You are " + age + " mars years old")
-  }
-
-  jupiterAge(number) {
-    let age = Math.round((number) / (11.86));
-  return ("You are " + age + " jupiter years old")
-  }
-}
-
-lifeExpectancy(number) {
-  
 }
